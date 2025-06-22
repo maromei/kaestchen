ff89cd4a8c3acf938a63462b46bf310809e8baed
maromei <maromei@proton.me>
2025-06-22 10:35:52 +0200

    Added Barebones sphinx documentation. Added build directory to .gitignore. Created the relevant hatch environment.

---
+++ docs/source/conf.py
@@ -0,0 +1,25 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project: str = "kaestchen"
+copyright: str = "2025, maromei"
+author: str = "maromei"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions: list[str] = ["myst_parser"]
+
+templates_path: list[str] = ["_templates"]
+exclude_patterns: list[str] = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme: str = "furo"
+html_static_path: list[str] = ["_static"]
