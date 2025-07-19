d58014ad88a1ced14f2e3517a85ea7217f61f6ee
maromei <maromei@proton.me>
2025-07-19 22:16:43 +0200

    Added an additional directive to mark a functin to be testing a named reature.

--- src/sphinx_feature_reference/__init__.py
+++ src/sphinx_feature_reference/__init__.py
@@ -3,6 +3,13 @@
 This extension introduces the `feature` role and directive.
 Currently it only displays the feature name in blue text.
 In the future it can be extended to do more complex tasks.
+
+Other possible tags should be:
+
+* ``feature-test``
+
+    * Can be used inline with the ID of a feature
+      in a docstring for a test function.
 """
 
 from sphinx.application import Sphinx
@@ -19,6 +26,7 @@ def setup(app: Sphinx) -> dict:
     """
 
     app.add_role("feature", FeatureReferenceRole())
+    app.add_role("feature-test", FeatureTestRole())
 
     return {
         "version": "0.1",
@@ -43,6 +51,25 @@ class FeatureReferenceRole(SphinxRole):
         """
         new_text: str = (
             f"Feature Reference: <span style='color: blue;'>{self.text}</span>"
+        )
+        inline_node: nodes.raw = nodes.raw("", new_text, format="html")
+        return [inline_node], []
+
+
+class FeatureTestRole(SphinxRole):
+    """Custom Sphinx role for marking a test function to be testing a feature."""
+
+    def run(self) -> tuple[list[nodes.Node], list[nodes.system_message]]:
+        """Mark a test function to be testing a feature.
+
+        Currently, it only wraps the text in a span with blue color.
+        Should be expanded in the future.
+
+        Returns:
+            tuple: A tuple containing the resulting nodes.
+        """
+        new_text: str = (
+            f"Test for Feature: <span style='color: blue;'>{self.text}</span>"
         )
         inline_node: nodes.raw = nodes.raw("", new_text, format="html")
         return [inline_node], []
